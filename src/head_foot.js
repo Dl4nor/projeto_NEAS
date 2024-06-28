cbb385507f1a8533335f88b515e210719d34d262
Ronald VS <ronald.silva10@fatec.sp.gov.br>
2024-06-28 05:20:34 -0300

    UPDATE apresentação

--- src/head_foot.js
+++ src/head_foot.js
@@ -6,8 +6,8 @@ $(function () {
         console.log(users)
         let userFound = false;
 
-        users.map((x) => {                
-            if(x.logado){
+        users.map((x) => {
+            if (x.logado) {
                 userName.innerHTML = x.nome;
                 userFound = true;
                 return
@@ -20,46 +20,65 @@ $(function () {
 
         userName.addEventListener("click", (x) => {
             users.map((x) => {
-                if(x.logado){
+                if (x.logado) {
                     x.logado = false;
                     localStorage.setItem("users", JSON.stringify(users));
                 }
             });
         });
+        
         // Função carrinho
         let jogo = [];
         let nomeJogo = [];
         let precoJogo = [];
-    
+        let countJogo = 0;
+        let carrinho = document.getElementById("carrinho");
+        let jogoJaPresente = false;
+
         for (let i = 0; i < 7; i++) {
             jogo[i] = document.getElementById("jogo" + (i + 1));
             nomeJogo[i] = document.getElementById("nomeJogo" + (i + 1));
             precoJogo[i] = document.getElementById("precoJogo" + (i + 1));
         }
-    
-        let carrinho = document.getElementById("carrinho");
-    
-        if (!carrinho) {
-            console.error("Elemento 'carrinho' não encontrado!");
-            return;
-        }
-    
+
+        
+
         function jogoToCarrinho(nome, preco) {
             const jogoCarrinho = document.createElement('a');
             jogoCarrinho.innerHTML = nome + ' - ' + preco;
             jogoCarrinho.setAttribute('href', '#');
+            jogoCarrinho.setAttribute('id', 'compra' + countJogo);
             carrinho.appendChild(jogoCarrinho);
         }
-    
+
+        function verificarRepetido(nome, preco) {
+            let jogosCarrinho = carrinho.querySelectorAll('a');
+            for (let i = 0; i < jogosCarrinho.length; i++) {
+                if (jogosCarrinho[i].innerHTML === nome + ' - ' + preco) {
+                    return true; // Retorna true se já encontrou um jogo igual no carrinho
+                }
+            }
+            return false; // Retorna false se o jogo não está no carrinho
+        }
+
         jogo.forEach((j, index) => {
             if (j) { // Verificando se o jogo não é null
-                j.addEventListener('click', function() {
+                j.addEventListener('click', function () {
                     const nome = nomeJogo[index] ? nomeJogo[index].innerText : "Nome desconhecido";
                     const preco = precoJogo[index] ? precoJogo[index].innerText : "Preço desconhecido";
-                    jogoToCarrinho(nome, preco);
+                    jogoJaPresente = verificarRepetido(nome, preco);
+                    
+                    if (!jogoJaPresente){
+                        jogoToCarrinho(nome, preco);
+                        countJogo += 1;
+                    }
+                    else {
+                        alert(nome + " já existe em seu carrinho");
+                    }
                 });
             }
         });
+
 
     });
 
