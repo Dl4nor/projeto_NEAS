da3ccc087b54d43f046f858e4d969bc0c3cb4ffb
Ronald VS <ronald.silva10@fatec.sp.gov.br>
2024-06-28 11:24:48 -0300

    Apresentação mode

--- Scripts/NEAS.js
+++ Scripts/NEAS.js
@@ -7,39 +7,35 @@ document.addEventListener('DOMContentLoaded', function() {
     let singOut = document.getElementById("login");
     var registeredUsers = [];
 
-    try {
-        var users = JSON.parse(localStorage.getItem("users"));
-    }
-    catch (error) {
-        //Registrando o usuário Admin
-        let nome = "admin";
-        let email = "admin";
-        let senha = "admin";
-        let telefone = "admin";
-        let endereco = "admin";
-        let isAdmin = true;
-        let logado = false;
+    //Registrando o usuário Admin
+    let nome = "admin";
+    let email = "admin";
+    let senha = "admin";
+    let telefone = "admin";
+    let endereco = "admin";
+    let isAdmin = true;
+    let logado = false;
+
+    let userInfo = {
+        'nome': nome,
+        'email': email,
+        'senha': senha,
+        'telefone': telefone,
+        'endereco': endereco,
+        'isAdmin': isAdmin,
+        'logado': logado
+    };
     
-        let userInfo = {
-            'nome': nome,
-            'email': email,
-            'senha': senha,
-            'telefone': telefone,
-            'endereco': endereco,
-            'isAdmin': isAdmin,
-            'logado': logado
-        };
-        
-        // registeredUsers = users;
-        registeredUsers.push(userInfo);
+    registeredUsers = users;
+    registeredUsers.push(userInfo);
+
+    console.log(userInfo);
+
+    var JSONregistered = JSON.stringify(registeredUsers)
+
+    localStorage.setItem("users", JSONregistered);
+
+    console.log(JSONregistered);
     
-        console.log(userInfo);
-    
-        var JSONregistered = JSON.stringify(registeredUsers)
-    
-        localStorage.setItem("users", JSONregistered);
-    
-        console.log(JSONregistered);
-    }
 
 });
