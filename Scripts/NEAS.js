de8dbfff22231c50757f1e3692671d0c809948fd
Ronald VS <111123357+Dl4nor@users.noreply.github.com>
2025-01-13 00:59:54 +0000

    Fix userNotDefined error?

--- Scripts/NEAS.js
+++ Scripts/NEAS.js
@@ -2,11 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
     // const nomeJogo = localStorage.getItem('lsNomeJogo')
     // const divJogo = localStorage.getItem('lsDiv');
 
+    let users = JSON.parse(localStorage.getItem("users")) || [];
 
-    
     let singOut = document.getElementById("login");
     var registeredUsers = [];
-
+    
     //Registrando o usuário Admin
     let nome = "admin";
     let email = "admin";
